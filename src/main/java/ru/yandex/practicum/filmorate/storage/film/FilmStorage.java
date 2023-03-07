3030f700f192adf0a2498ae01df369a1a4e803a1
Primorets <Buholia@yandex.ru>
2023-03-07 23:43:47 +0300

    fix: устранены конфликты

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -35,10 +35,4 @@ public interface FilmStorage {
 
     List<Film> searchFilmsByDirector(String query);
 
-    List<Film> searchFilmsByNameOrDirector(String query);
-
-    List<Film> searchFilmsByName(String query);
-
-    List<Film> searchFilmsByDirector(String query);
-
 }
